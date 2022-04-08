89eca067bd0786d41c7381395e17b06949d6c2cc
kazishuvo22 <kazishuvomain@gmail.com>
2022-04-09 01:15:42 +0600

    update

--- ProminenceProject/ProminenceApp/models.py
+++ ProminenceProject/ProminenceApp/models.py
@@ -18,7 +18,7 @@ class General(models.Model):
                                         help_text="Only PNG, JPG, JPEG format supported",
                                         validators=[FileExtensionValidator(
                                             allowed_extensions=['png', 'jpg', 'jpeg'])])
-    last_author = models.ForeignKey(User, on_delete=models.DO_NOTHING, auto_created=True)
+    last_author = models.ForeignKey(User, on_delete=models.DO_NOTHING, editable=False)
     home_video_link = models.CharField(max_length=250, verbose_name="Enter youtube link for home video",
                                        help_text="Add 'http:// or https://' before your web address if not inserted, "
                                                  "Example:(http://example.com)")
@@ -65,7 +65,7 @@ class About(models.Model):
                                         help_text="Only PNG, JPG, JPEG format supported",
                                         validators=[FileExtensionValidator(
                                             allowed_extensions=['png', 'jpg', 'jpeg'])])
-    last_author = models.ForeignKey(User, on_delete=models.DO_NOTHING, auto_created=True)
+    last_author = models.ForeignKey(User, on_delete=models.DO_NOTHING, editable=False)
     last_edited = models.DateTimeField(auto_now=True)
 
     def save(self, **kwargs):
